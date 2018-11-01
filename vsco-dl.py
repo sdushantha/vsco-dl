c361ac18e434446b0fc34db6c1f6aa8484be0184
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2018-11-01 09:41:11 +0100

    fixed issue where it would not work

--- vsco-dl.py
+++ vsco-dl.py
@@ -7,10 +7,17 @@ import sys
 from random import random
 import argparse
 
+# This magic spell lets me erase the current line. 
+# I can use this to show for example "Downloading..."
+# and then "Downloaded" on the line where 
+# "Downloading..." was.  
+ERASE_LINE = '\x1b[2K'
+CURSOR_UP_ONE = '\x1b[1A'
 
 def download(username, html):
-	a = html.split("window.__PRELOADED_STATE__ = ", 1)[1]
-	data = a.split("\n",1)[0]
+
+	# This is extracts the JSON data from the HTML page
+	data = re.findall('window.__PRELOADED_STATE__ = (.*?)</script>', html)[0]
 
 	array = json.loads(data).get("medias").get("byId")
 
@@ -25,12 +32,19 @@ def download(username, html):
 	    # Need to have https:// to have valid url
 	    file_url = "https://"+file_url
 	    
+	    # The file name is just some random number and the filename
+	    # gotton from the url. That is because some of the files
+	    # have the same names and this will cause issues
 	    fname = str(random())[3:9]+file_url.split("/")[-1]
 	    dir_name = username+"/"
 	    path = dir_name+fname
 
 	    urllib.request.urlretrieve(file_url, path)
-	    print("\033[92m[+] Downloaded:\033[0m {}".format(path.replace(dir_name, "")))
+
+	    # This will let me show the output on the same line
+	    print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE)
+
+	    print("\033[92m✔ Downloaded:\033[0m {}".format(path.replace(dir_name, "")))
 
 
 def main():
@@ -48,18 +62,21 @@ def main():
 
 
 	for page in range(args.pages):
-		url = url ="https://vsco.co/{}/images/{}"
+		url ="https://vsco.co/{}/images/{}"
 
 		r = requests.get(url.format(args.username, page+1))
 
 		if r.status_code == 404:
-			print("\033[91m[-] Invalid username\033[0m")
+			print("\033[91m✘ Invalid username\033[0m")
 			sys.exit()
 
-		print("\033[92m[+] Fetched data\033[0m")
+		print("\033[92m✔ Fetched data\n\033[0m")
 		os.makedirs(args.username, exist_ok=True)
 		html = r.text
 
 		download(username=args.username, html=html)
+		print(CURSOR_UP_ONE + ERASE_LINE + CURSOR_UP_ONE + CURSOR_UP_ONE)
+
+	print("\033[92m✔\033[0m Download is complete")
 
 main()
